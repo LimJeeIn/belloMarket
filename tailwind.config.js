83f7d6da48aac2f2c7929830a562999039fb2996
limjeein <jeein777@naver.com>
2023-08-28 22:56:46 +0900

    :lipstick:Style : 배너 UI 추가 #7

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         brand: '#21BF48',
       },
+      backgroundImage: {
+        banner: `url('../public/images/banner.jpg')`,
+      },
     },
   },
   plugins: [],

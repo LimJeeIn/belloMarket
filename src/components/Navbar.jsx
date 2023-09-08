bbc64905f9a6c7c9a42257154a6c6f0978aadc46
limjeein <jeein777@naver.com>
2023-09-08 22:32:40 +0900

    이전으로 되돌아가기

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import User from './User';
 import Button from './ui/Button';
 import { useAuthContext } from '../context/AuthContext';
 import CartStatus from './CartStatus';
-import logo from '../assets/image/logo.png';
 
 export default function Navbar() {
   const { user, login, logout } = useAuthContext();
@@ -13,7 +12,7 @@ export default function Navbar() {
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
-        <img src={logo} alt="logo" />
+        <h2>Bello market</h2>
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>

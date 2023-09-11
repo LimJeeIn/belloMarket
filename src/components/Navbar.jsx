a522b055b5360bf7ef91871810468200ae5e4b03
limjeein <jeein777@naver.com>
2023-09-11 23:16:59 +0900

    h1 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,9 @@ export default function Navbar() {
 
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
-      <Link to="/" className="flex items-center text-4xl text-brand"></Link>
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <h1>Bello Market</h1>
+      </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         {user && (

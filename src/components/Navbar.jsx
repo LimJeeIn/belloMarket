b960cdf8be39d0135a6b949c35a56afb47804405
limjeein <jeein777@naver.com>
2023-09-15 23:03:20 +0900

    :lipstick:Style : UI 수정중 #30

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FcAbout } from 'react-icons/fc';
+import { BiEditAlt } from 'react-icons/bi';
 import User from './User';
 import Button from './ui/Button';
 import { useAuthContext } from '../context/AuthContext';
@@ -10,11 +10,11 @@ export default function Navbar() {
   const { user, login, logout } = useAuthContext();
 
   return (
-    <header className="flex justify-between border-b border-gray-300 p-2">
+    <header className="flex justify-between border-b border-gray-300 max-w-screen-3xl px-6 h-[5.5rem]">
       <Link to="/" className="flex items-center text-4xl text-brand">
         <h1>Bello Market</h1>
       </Link>
-      <nav className="flex items-center gap-4 font-semibold">
+      <nav className="flex items-center gap-5 font-semibold text-xs">
         <Link to="/products">Products</Link>
         {user && (
           <Link to="/carts">
@@ -23,7 +23,7 @@ export default function Navbar() {
         )}
         {user && user.isAdmin && (
           <Link to="/products/new" className="text-2xl">
-            <FcAbout />
+            <BiEditAlt />
           </Link>
         )}
         {user && <User user={user} />}

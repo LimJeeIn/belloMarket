a33cb5febc0b0e3a698e0c68a4c6723436e027b3
limjeein <jeein777@naver.com>
2023-09-21 22:20:01 +0900

    :sparkles:Feat : 전체 tailwind css 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Banner() {
   return (
     <section className="h-screen relative overflow-hidden">
-      <video
+      {/* <video
         playsInline
         autoPlay
         muted
@@ -11,7 +11,7 @@ export default function Banner() {
         className="absolute top-0 left-0 min-w-full min-h-full w-auto h-auto object-cover"
       >
         <source src="/images/running_shoes.mp4" type="video/mp4" />
-      </video>
+      </video> */}
     </section>
   );
 }
